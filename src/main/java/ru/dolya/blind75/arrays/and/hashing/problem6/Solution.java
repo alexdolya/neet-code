83109098d9fd782f73a32feb0b2c7253f2ddd6a4
alexdolya <alexdolya90@gmail.com>
2025-11-06 09:12:57 +0400

    problem 6 solved

---
+++ src/main/java/ru/dolya/blind75/arrays/and/hashing/problem6/Solution.java
@@ -0,0 +1,49 @@
+package ru.dolya.blind75.arrays.and.hashing.problem6;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        List<String> strings = List.of("neet", "code", "love", "you");
+
+        String encode = encode(strings);
+        System.out.println(encode);
+
+        System.out.println(decode(encode));
+    }
+
+    public static String encode(List<String> strs) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String string : strs) {
+            stringBuilder.append(string.length())
+                    .append("$")
+                    .append(string);
+        }
+        return stringBuilder.toString();
+    }
+
+    public static List<String> decode(String str) {
+        if (str.isEmpty()) {
+            return Collections.emptyList();
+        }
+        List<String> result = new ArrayList<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != '$') {
+                stringBuilder.append(str.charAt(i));
+            } else {
+                int wordLength = Integer.parseInt(stringBuilder.toString());
+                int beginIndex = i + 1;
+                int endIndex = beginIndex + wordLength;
+                String substring = str.substring(beginIndex, endIndex);
+                result.add(substring);
+                stringBuilder = new StringBuilder();
+                i = endIndex - 1;
+            }
+        }
+        return result;
+    }
+}
