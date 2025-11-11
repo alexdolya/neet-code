93eb6203ff4fc85642c62178351dcffa0f463480
alexdolya <alexdolya90@gmail.com>
2025-11-11 09:35:33 +0400

    problem 8 solved

---
+++ src/main/java/ru/dolya/blind75/arrays/and/hashing/problem8/Solution.java
@@ -0,0 +1,42 @@
+package ru.dolya.blind75.arrays.and.hashing.problem8;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 20, 4, 10, 3, 4, 5};
+
+        System.out.println(longestConsecutive(nums));
+
+        int[] nums2 = new int[]{0, 0};
+
+        System.out.println(longestConsecutive(nums2));
+
+    }
+
+    public static int longestConsecutive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        Set<Integer> set = new TreeSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int maxSeq = 1;
+        for (int num : nums) {
+            int currentMaxSeq = 1;
+            int currentNum = num + 1;
+            while (set.contains(currentNum)) {
+                currentMaxSeq++;
+                currentNum++;
+            }
+            if (currentMaxSeq > maxSeq) {
+                maxSeq = currentMaxSeq;
+            }
+        }
+
+        return maxSeq;
+    }
+}
