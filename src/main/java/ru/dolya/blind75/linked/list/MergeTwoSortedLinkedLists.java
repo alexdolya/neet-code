43b029100f65a3a92f2d5544124239cf58dd2bb9
alexdolya <alexdolya90@gmail.com>
2026-01-16 10:17:25 +0400

    remove comments

--- src/main/java/ru/dolya/blind75/linked/list/MergeTwoSortedLinkedLists.java
+++ src/main/java/ru/dolya/blind75/linked/list/MergeTwoSortedLinkedLists.java
@@ -37,15 +37,3 @@ public class MergeTwoSortedLinkedLists {
         return mock.next;
     }
 }
-
-
-//1 -> 2 -> 4
-//1 -> 3 -> 5
-
-//1 -> 1 -> 2 -> 3 -> 4 -> 5
-
-
-//1 -> 2 -> 3
-//9 -> 10 -> 11
-
-//1 -> 2 ->
