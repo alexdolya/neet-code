960c6a34c3d9a73d15799cad3f8729f1bb38a8d0
alexdolya <alexdolya90@gmail.com>
2026-01-26 11:51:06 +0400

    LowestCommonAncestorInBinarySearchTree add second solve

--- src/main/java/ru/dolya/blind75/trees/LowestCommonAncestorInBinarySearchTree.java
+++ src/main/java/ru/dolya/blind75/trees/LowestCommonAncestorInBinarySearchTree.java
@@ -10,10 +10,32 @@ public class LowestCommonAncestorInBinarySearchTree {
     public static void main(String[] args) {
         TreeNode tree = createTree(new Integer[]{5, 3, 8, 1, 4, 7, 9, null, 2});
         TreeNode treeNode = lowestCommonAncestor(tree, tree.left, tree.right);
+        TreeNode treeNodeB = lowestCommonAncestorBinary(tree, tree.left, tree.right);
 
         TreeNode tree1 = createTree(new Integer[]{5, 3, 8, 1, 4, 7, 9, null, 2});
         TreeNode treeNode1 = lowestCommonAncestor(tree1, tree1.left, tree1.left.right);
+        TreeNode treeNode1B = lowestCommonAncestorBinary(tree1, tree1.left, tree1.left.right);
+
+        System.out.println();
     }
+
+    public static TreeNode lowestCommonAncestorBinary(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+
+        while (root != null) {
+            if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } else  if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else {
+                return root;
+            }
+        }
+        return null;
+    }
+
 
     public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null) {
